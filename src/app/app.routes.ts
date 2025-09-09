a987ad87bf54b20c90967bef0506c58df06ef287
Your Name <ekka.beaula@gmail.com>
2025-09-09 19:32:56 +0200

    layoutcomponent fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { EmployeeComponent } from './pages/employee/employee.component';
+import { LayoutComponent } from './pages/layout/layout.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
     path:'',
-    component:Layout,
+    component:LayoutComponent,
     children:[
       { path: 'dashboard', component: DashboardComponent },
       { path: '**', redirectTo: '/login' },
