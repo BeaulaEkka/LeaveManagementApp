9c80d9ca2b91b2e2f16e46ed51e22b14ba37cf85
Your Name <ekka.beaula@gmail.com>
2025-09-09 19:15:05 +0200

    employee page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path:'',
+    component:Layout,
+    children:[
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '**', redirectTo: '/login' },
+      {path:'',component:EmployeeComponent}
+    ]
+  }
+
+
+];
